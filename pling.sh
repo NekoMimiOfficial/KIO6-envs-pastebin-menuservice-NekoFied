9ae5d4e5fd44e97a75c6490ae96a1efc04038e53
NekoMimiOfficial <nekomimi@tilde.team>
2025-05-07 15:57:13 +0300

    completed pling files

--- pling.sh
+++ pling.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+rm $HOME/.local/share/kio/servicemenus/UploadToEnvs.desktop
+
 git clone --depth=1 https://github.com/NekoMimiOfficial/KIO6-envs-pastebin-menuservice-NekoFied && cd KIO6-envs-pastebin-menuservice-NekoFied && chmod +x install.sh && ./install.sh && cd .. && rm -rf KIO6-envs-pastebin-menuservice-NekoFied
