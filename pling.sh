d06a465df8feb7f747117ffc7057a57ecf57918f
NekoMimiOfficial <nekomimi@tilde.team>
2025-05-07 15:54:32 +0300

    added pling installer

---
+++ pling.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/NekoMimiOfficial/KIO6-envs-pastebin-menuservice-NekoFied && cd KIO6-envs-pastebin-menuservice-NekoFied && chmod +x install.sh && ./install.sh && cd .. && rm -rf KIO6-envs-pastebin-menuservice-NekoFied
